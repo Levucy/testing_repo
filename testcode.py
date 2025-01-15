72bec4254b7120aa19e6bd39ab7208862ed61a52
Лев Александрович <levucy0@gmail.com>
2025-01-15 20:51:22 +0300

    тест4

--- testcode.py
+++ testcode.py
@@ -3,10 +3,10 @@ import random
 COUNTRIES = ['Республика Беларусь', 'Российская Федерация', 'Республика Казахстан', 'Республика Армения']
 
 def random_country():
-    print(random.choice(COUNTRIES))
+    return random.choice(COUNTRIES)
 
 def main():
     for i in range(random.randint(1, 7)):
-        random_country()
+        print(f'''Название: {random_country()}''')
 
 main()
