2b45a0abbb785e4bc74180379e638bcde7a9dfeb
Лев Александрович <levucy0@gmail.com>
2025-01-15 21:08:37 +0300

    v0.0.0

--- testcode.py
+++ testcode.py
@@ -1,12 +1,18 @@
 import random
 
-COUNTRIES = ['Республика Беларусь', 'Российская Федерация', 'Республика Казахстан', 'Республика Армения']
+COUNTRIES = ['Республика Беларусь', 'Российская Федерация', 'Республика Казахстан', 'Республика Армения',
+             'Республика Сербия']
+LEADERS = ['Владимир Владимирович Путин', 'Александр Вучич', 'Александр Григорьевич Лукашенко',
+           'Гарникович Ваагн Хачатурян', 'Кемелевич Касым-Жомарт Токаев']
 
 def random_country():
     return random.choice(COUNTRIES)
 
+def random_leader():
+    return random.choice(LEADERS)
+
 def main():
-    for i in range(random.randint(1, 7)):
-        print(f'''Название: {random_country()}''')
+    for i in range(random.randint(2, 5)):
+        print(f'''Страна: {random_country()} | Президент: {random_leader()}''')
 
 main()
